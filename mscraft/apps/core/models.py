9474c1aa221ff7d2c6e29be11679a9c25ce0f0ca
Accalina <accalina@localhost.localdomain>
2020-10-18 19:47:59 +0700

    Added Engine: Battlelog, Buy & Sell unit, player and falled heroes info

--- mscraft/apps/core/models.py
+++ mscraft/apps/core/models.py
@@ -1,6 +1,17 @@
+
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
+class Player(models.Model):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    cash        = models.IntegerField(null=False, default=300)
+    joindate    = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
 class Market(models.Model):
     name    = models.CharField(max_length=30, null=False)
     desc    = models.TextField(blank=True)
@@ -24,9 +35,10 @@ class Equipment(models.Model):
         return f"{self.name}"
 
 class Member(models.Model):
+    player  = models.ForeignKey(Player, on_delete=models.CASCADE) 
     name    = models.CharField(max_length=30, null=False)
-    unit    = models.OneToOneField(Market, on_delete=models.CASCADE)
-    equip   = models.OneToOneField(Equipment, on_delete=models.CASCADE)
+    unit    = models.ForeignKey(Market, on_delete=models.CASCADE)
+    equip   = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     active  = models.BooleanField(null=False, default=True)
 
     def __str__(self):
@@ -38,6 +50,7 @@ class Enemy(models.Model):
     health  = models.IntegerField(null=False, default=0)
     atkmin  = models.IntegerField(null=False, default=0)
     atkmax  = models.IntegerField(null=False, default=0)
+    price   = models.IntegerField(null=False, default=0)
     sprite  = models.TextField(blank=True)
 
     def __str__(self):
--- mscraft/apps/core/views.py
+++ mscraft/apps/core/views.py
@@ -1,5 +1,5 @@
-# from django.shortcuts import render
 
+import random
 from rest_framework import filters
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -9,14 +9,16 @@ from .models import (
     Market,
     Equipment,
     Member,
-    Enemy
+    Enemy,
+    Player
 )
 
 from .serializers import (
     MarketSerializer,
     EquipmentSerializer,
     MemberSerializer,
-    EnemySerializer
+    EnemySerializer,
+    PlayerSerializer
 )
 
 
@@ -36,7 +38,7 @@ class EquipmentAPIView(ModelViewSet):
 class MemberAPIView(ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
-    search_fields = ['name']
+    search_fields = ['player__id']
     filter_backends = [filters.SearchFilter]
 
 class EnemyAPIView(ModelViewSet):
@@ -45,17 +47,128 @@ class EnemyAPIView(ModelViewSet):
     search_fields = ['name']
     filter_backends = [filters.SearchFilter]
 
+class PlayerAPIView(ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    search_fields = ['id']
+    filter_backends = [filters.SearchFilter]
 
-class Test(APIView):
-    def get(self, request, name=None):
-        personel_list = [personel.name for personel in Market.objects.all()]
-        return Response({'personel':personel_list, 'name': name})
+class GenerateBattle(APIView):
+    """
+        This endpoint is used for generating RnG Battle log
+        Use the POST request to generate the battlelog.
 
-    def post(self, request, name=None):
+        Parameters:
+            playerid    : <int>,
+            memberid    : <int>,
+            enemyid     : <int>
+    """
+    def get(self, request): 
+        return Response({"msg": "This endpoint is used for generating RnG Battle log"})
+
+    def post(self, request):
         """
-        Todo: still not found how to inject parameter
+            Parameters:
+                playerid    : <int>,
+                memberid    : <int>,
+                enemyid     : <int>
+            Return:
+                --
         """
-        return Response({"data": self.request.data})
-        # personel = Member.objects.filter(id=personel_id)
-        # enemy_data = Enemy.objects.filter(id=enemy_id)
-        # return Response({"member": personel_data.name, "enemy": enemy_data.name})
+        report = []
+        winner = ""
+        battle = True
+        data = self.request.data
+        p = Player.objects.get(id=data.get('playerid'))
+        m = Member.objects.get(id=data.get('memberid'))
+        e = Enemy.objects.get(id=data.get('enemyid'))
+
+        while battle:
+            # Member turn to attack
+            m_atk       = random.randint(m.unit.atkmin, m.unit.atkmax)
+            e.health    = e.health - m_atk
+            report.append(f"{m.name} Attack inflict {m_atk} damage, {e.name} have {e.health} HP left")
+            if e.health <= 0:
+                battle = False
+                report.append("Battle is over")
+                winner = f"Member {m.name}"
+                wincash = e.price + p.cash
+                p.cash = wincash
+                p.save()
+                break
+            # Enemy turn to attack
+            e_atk           = random.randint(e.atkmin, e.atkmax)
+            m.unit.health   = m.unit.health - e_atk
+            report.append(f"{e.name} Attack inflict {e_atk} damage, {m.name} have {e.health} HP left")
+            if m.unit.health <= 0:
+                battle = False
+                report.append("Battle is over")
+                winner = f"Enemy {h.name}"
+                # m.status = False
+                # m.save()
+                break
+        return Response({"report": report, "winner": winner, 'memberSprinte': m.unit.sprite, 'enemySprite': e.sprite})
+
+class BuyMember(APIView):
+    """
+        This endpoint is used for Buy new member from market
+        Parameters:
+            unitname    : <str>,
+            playerid    : <int>,
+            marketid    : <int>,
+    """
+    def get(self, request): 
+        return Response({"msg": "This endpoint is used for Buy new member from market"})
+
+    def post(self, request):
+        data = self.request.data
+        ply = Player.objects.get(id=data.get('playerid'))
+        mar = Market.objects.get(id=data.get('marketid'))
+        equ = Equipment.objects.get(id=1)
+        unitname = data.get('unitname')
+
+        if ply.cash >= mar.price:
+            # Player has sufficient cash
+            ply.cash = ply.cash - mar.price
+            Member.objects.create(
+                name=unitname,
+                player=ply,
+                unit=mar,
+                equip=equ,
+                active=True
+            )
+            ply.save()
+            return Response({"msg": f"New member {unitname} has join", "success": True})
+        return Response({"msg": "insufficient funds", "success": False})
+
+class SellMember(APIView):
+    """
+        This endpoint is used for Sell old member to the market
+        Parameters:
+            playerid    : <int>,
+            memberid    : <int>,
+    """
+    def get(self, request): 
+        return Response({"msg": "This endpoint is used for Sell old member to the market"})
+
+    def post(self, request):
+        try:
+            data = self.request.data
+            ply = Player.objects.get(id=data.get('playerid'))
+            m_data = Member.objects.get(id=data.get('memberid'))
+            ply.cash = ply.cash + m_data.unit.price
+            ply.save()
+            m_data.delete()
+            return Response({"msg": f"Member sold back to the market", "success": True})
+        except apps.core.models.Member.DoesNotExist:
+            return Response({"msg": f"No Member with that ID", "success": False})
+
+class FallenHero(APIView):
+    """
+        This endpoint is used for display deactivated member
+        Parameters:
+            playerid    : <int>,
+    """
+    def get(self, request, playerid):
+        fallenList = [m.name for m in Member.objects.filter(player=playerid, active=False)]
+        return Response({"Heroes": fallenList})
